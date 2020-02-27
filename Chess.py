2435e2cb30ac96f566087b8f34a5cb50bf947375
Bradley <35861770+apoptosus@users.noreply.github.com>
2020-02-27 05:54:14 -0800

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -1,7 +1,6 @@
 import pygame
 from math import ceil
 
-
 # Define the colors we will use in RGB format
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -11,15 +10,17 @@ green = (100,200,92)
 blue = (0,0,255)
 red = (255,0,0)
 
+# Counter for White - Black turn
 turn_count = 0
 
-
+# Generate tuples for the initialization of pieces into Piece class
 white_pieces = ('12Pawn','22Pawn','32Pawn','42Pawn','52Pawn','62Pawn','72Pawn','82Pawn','11Rook','21knight','31Bishop','41Queen','51King','61Bishop','71knight','81Rook')
 black_pieces = ('17Pawn','27Pawn','37Pawn','47Pawn','57Pawn','67Pawn','77Pawn','87Pawn','18Rook','28knight','38Bishop','48Queen','58King','68Bishop','78knight','88Rook')
 
+# This list holds all of the Piece class objects
 pieces = list()
 
-
+#Piece class holding name, color, position, and an identifier of pieces
 class Piece():
     def __init__(self, name, color, position, symbol):
         self.name = name # 31bishop
@@ -30,12 +31,12 @@ class Piece():
     def __str__(self):
         return str(self.name) + ',' + str(self.color) + ',' + str(self.position) + ',' + str(self.symbol)
 
-
-
+# Board class that focuses on drawing and updating the pygame board
 class Board():
     def __init__(self,pieces):
         self.pieces = pieces
-
+    
+    # Fill in squares of board using 2D array
     def draw_squares(self):
         color_shift = 0
         square_size = 50
@@ -48,20 +49,27 @@ class Board():
                 color_shift += 1
             color_shift -= 1
 
-
+    # Loop through pieces and draw them on board based on their 'position'
     def draw_pieces(self,pieces):
         gamegrid = [[0 for x in range(1, 9)] for y in range(1, 9)]
 
         for i in pieces:
+            # a position of 0 means that the piece has been captured
             if i.position == 0:
                 continue
             x = int(i.position[0])-1
             y = int(i.position[1])-1
             gamegrid[x][y] = i.symbol
+            
+            # Create a screen layer above the gameboard that pieces are drawn onto
             piece_screen = myfont.render(i.symbol, False, i.color)
+            # Draw the pieces onto the screen
             screen.blit(piece_screen, ((x * 50) + 10, (y * 50) + 10))
 
+# Class that handles game logic, mouse selection, and progression of turns
 class Turn():
+    
+    # Display whose turn it is and which turn it is
     def begin_turn(self):
         turns = myfont1.render('Turn: ' + str(turn_count + 1), False, blue)
         screen.blit(turns, (0, 480))
@@ -73,33 +81,36 @@ class Turn():
             bottom_text = myfont.render("Black's Turn", False, gray)
             screen.blit(bottom_text, (90, 450))
             return black
-
+    
+    # Takes position (x,y) of mouse on MOUSEBUTTONUP (og_square) and MOUSEBUTTONDOWN (destination_square events and maps them to gameboard squares
     def mouse_position_translator(self,og_square,destination_square):
         for i in range(2):
+            
             og_square[i] = ceil(og_square[i]/50)
             destination_square[i] = ceil(destination_square[i]/50)
 
         return og_square,destination_square
 
+    # Confirm piece position change with legality
     def piece_position_switch(self,og_square,destination_square):
-        #Here is the position of the original square
+        # The position of the original square
         old_position = str(og_square[0]) + str(og_square[1])
         new_position = str(destination_square[0])+str(destination_square[1])
 
-        #let's determine if the position has a piece in it
+        # Determine if the position has a piece in it
         for i in pieces:
             if old_position == i.position:
-            #if it does, then lets take the piece from that position and put it to the new position
-                legal = new_turn.check_moves(i, new_position) # lets check to see if that move is legal in the wonderful world of chess
+            # If so, take the piece from that position and put it to the new position
+                legal = new_turn.check_moves(i, new_position) # Check legality
                 if legal == True:
                     for j in pieces:
-                        if j.name != i.name and j.position == new_position:
+                        # If two pieces overlap after legality check, then the taken piece's position becomes '00'
+                        if j.name != i.name and j.position == new_position: 
                             j.position = '00'
                     i.position = new_position
                     return True
-
-
-
+                
+    # Match piece legality checks to the type of piece being moved
     def check_moves(self,piece,new_position):
         if new_turn.begin_turn() != piece.color:
             return False
@@ -119,13 +130,18 @@ class Turn():
         if piece.symbol == 'K':
             return new_turn.king_moves(piece,new_position)
 
-
+ 
+    
+    """The logic of the *piece*_moves() methods should be improved at a later date. Currently for each possible rule,
+    the legality functions check the piece location, 
+    and all other pieces in all possible directions that the piece can move in in the 2D plane, one by one, for each color."""
+    
+    # Pawn legal moves
     def pawn_moves(self,piece,new_position):
         x = int(piece.position[0])
         y = int(piece.position[1])
         newx = int(new_position[0])
         newy = int(new_position[1])
-
 
         if abs(newy-y) > 2:
             return False
@@ -136,16 +152,12 @@ class Turn():
                 return False
         if piece.color == white:
             for c in range(y+1, newy+1):
-                for i in pieces:
-                    print(i)
+                for i in pieces:                 
                     if int(i.position[1]) == c: #checks if any piece is in the line of pawn move
                         if int(i.position[0]) == x:
                             return False
                     for i in pieces:
-                        print('i want to make it')
                         if int(i.position[1]) == c:
-                            print(i)
-                            print(c)
                             if int(i.position[0]) == (x+1) or int(i.position[0]) == (x-1):
                                 if i.position == new_position:
                                     return True
@@ -155,26 +167,21 @@ class Turn():
             return True
 
         if piece.color == black:
-            for c in range(newy, y):
-                print (c)
-                for i in pieces:
-                    print(i)
+            for c in range(newy, y):                
+                for i in pieces:                  
                     if int(i.position[1]) == c:  # checks if any piece is in the line of pawn move
                         if int(i.position[0]) == x:
                             return False
                     for i in pieces:
-                        print('i want to make it')
                         if int(i.position[1]) == c:
-                            print(i)
-                            print(c)
                             if int(i.position[0]) == (x + 1) or int(i.position[0]) == (x - 1):
                                 if i.position == new_position:
                                     return True
                     if newx - x != 0:  # if pawn moves left or right, bye
                         return False
-
             return True
 
+    
     def knight_moves(self,piece,new_position):
 
         if piece.color == white:
@@ -189,14 +196,14 @@ class Turn():
                         return False
         if int(piece.position[0]) == int(new_position[0]) - 1 or int(piece.position[0]) == int(new_position[0]) + 1:
             if int(piece.position[1]) == int(new_position[1]) - 2 or int(piece.position[1]) == int(new_position[1]) + 2:
-                print('what on earth')
-                return True # add a take function
+                return True 
         if int(piece.position[0]) == int(new_position[0])-2 or int(piece.position[0]) == int(new_position[0]) + 2:
             if int(piece.position[1]) == int(new_position[1]) - 1 or int(piece.position[1]) == int(new_position[1]) + 1:
-                return True # add a take function
+                return True 
         else:
             return False
 
+        
     def bishop_moves(self,piece,new_position):
         x = int(piece.position[0])
         y = int(piece.position[1])
@@ -208,17 +215,13 @@ class Turn():
             return False
 
         if (newx - x) != (newy - y) and (newx-x) != (y-newy):
-            print('major tenent wrong')
             return False
 
         if x > newx and y > newy:
             #direction (-,-)
             xcount = newx
             for ycount in range(newy, y):
-                print(xcount,ycount)
-
                 for i in pieces:
-
                     if int(i.position[1]) == ycount:
                         if int(i.position[0]) == xcount:
                             if i.color == piece.color:
@@ -239,9 +242,7 @@ class Turn():
             #direction (-,+)
             xcount = newx
             for ycount in range(newy, y):
-
                 for i in pieces:
-
                     if int(i.position[1]) == ycount:
                         if int(i.position[0]) == xcount:
                             if i.color == piece.color:
@@ -262,9 +263,7 @@ class Turn():
             #direction (+, -)
             xcount = newx
             for ycount in range(newy, y):
-
                 for i in pieces:
-
                     if int(i.position[1]) == ycount:
                         if int(i.position[0]) == xcount:
                             if i.color == piece.color:
@@ -280,8 +279,6 @@ class Turn():
                         return True
             if count <= 1:
                 return False
-
-
 
         if x < newx and y < newy:
             #direction (+,+)
@@ -333,7 +330,6 @@ class Turn():
                         return True
             if count <= 1:
                 return False
-
 
         if x == newx and newy < y:
             #direction = '-y'
@@ -404,8 +400,6 @@ class Turn():
             if new_position == i.position:
                 if i.color == piece.color:
                     return False
-        print('Praise be the oligarchy, until the damnest day I die')
-
         if x-1 <= newx <= x+1 and y-1 <= newy <= y+1:
             return True
 
@@ -416,7 +410,7 @@ class Turn():
 
 
 
-#Define Class-Objects
+#Define Piece Class-Objects
 for i in white_pieces:
     i = Piece(i, white, i[0:2], i[2])
     pieces.append(i)
@@ -424,94 +418,95 @@ for i in black_pieces:
     i = Piece(i, black, i[0:2], i[2])
     pieces.append(i)
 
+#Initialize GameBoard and Turns
 gameboard = Board(pieces)
 new_turn = Turn()
 
 #set up pygame
 pygame.init()
 
+# Fonts for blitting
 myfont = pygame.font.SysFont('Comic Sans MS', 50)
 myfont1 = pygame.font.SysFont('Comic Sans MS', 20)
 
-##Sets up screens
+# Screens for displaying
 screen = pygame.display.set_mode((400, 500))
 board_screen = pygame.Surface((400, 400))
 piece_screen = pygame.Surface((400, 400))
 
-gameboard.draw_squares()
+# Draw gameboard
+gameboard.draw_squares() # Draws squares onto board_screen
 screen.blit(board_screen, (0, 0))
 
-gameboard.draw_pieces(pieces)
+gameboard.draw_pieces(pieces) # Draws pieces onto piece_screen
 
-
-"""GAME LOOP BEGINS HERE"""
+### GAME LOOP BEGINS HERE ###
 clock = pygame.time.Clock()
+
 
 done = False
 while not done:
+    # Loops permanently unless quit event is returned
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+          
+    # Redraw board and pieces each round
     screen.blit(board_screen, (0, 0))
     gameboard.draw_pieces(pieces)
+    
+    # Check to see if king has been taken
     for i in pieces:
         if i.symbol == 'K' and i.position == '00':
+            # If so, CHECKMATE!
             checkmate = pygame.font.SysFont('Comic Sans MS', 100)
             bottom_text = checkmate.render("CHECKMATE", False, red)
             screen.blit(bottom_text, (0, 150))
+    
+    # White/Black turn display
     new_turn.begin_turn()
+    # Quick fix: Puts white on the correct side
     pygame.display.flip()
-    '''RIGHT  HERE WE NEED A MORE ACCURATE CLICK RECEIVER AND PROMPT'''
+    
     if turn_count %2 == 0:
         print("White to select a move")
     else:
         print("Black to select a move")
-
-
-    while event.type!= pygame.MOUSEBUTTONDOWN:
-        event = pygame.event.wait()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mousex = int(pygame.mouse.get_pos()[0])
-            mousey = int(pygame.mouse.get_pos()[1])
-            og_square = [mousex, mousey]
-
-    while event.type!= pygame.MOUSEBUTTONUP:
-        event = pygame.event.wait()
-        if event.type == pygame.MOUSEBUTTONUP:
-            mousex = int(pygame.mouse.get_pos()[0])
-            mousey = int(pygame.mouse.get_pos()[1])
-            destination_square = [mousex,mousey]
-
-    new_turn.mouse_position_translator(og_square, destination_square)
-
-    print("checking the legality of the move...")
-    legality = new_turn.piece_position_switch(og_square, destination_square)
-
-    while legality != True:
-        #Check when a mouse is clicked
+    
+    # Check for legality: If True, move pieces
+    legality = None
+    while legality != True
+        # Wait for mouse click
         while event.type != pygame.MOUSEBUTTONDOWN:
             event = pygame.event.wait()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mousex = int(pygame.mouse.get_pos()[0])
                 mousey = int(pygame.mouse.get_pos()[1])
+                # Store position where mouse is clicked on screen
                 og_square = [mousex, mousey]
-        # when mouse is released take its coordinates
-        while event.type != pygame.MOUSEBUTTONUP:
+
+        # Wait for mouse button release
+        while event.type!= pygame.MOUSEBUTTONUP:
             event = pygame.event.wait()
             if event.type == pygame.MOUSEBUTTONUP:
                 mousex = int(pygame.mouse.get_pos()[0])
                 mousey = int(pygame.mouse.get_pos()[1])
-                destination_square = [mousex, mousey]
-        #reformat them to match the board layout
+
+                # Store release position
+                destination_square = [mousex,mousey]
+
+        # Translate stored positions to board square positions
         new_turn.mouse_position_translator(og_square, destination_square)
 
-        print("waiting for a legal move...")
+        print("checking the legality of the move...")
+        # Checks whether or not a move can be legally made
         legality = new_turn.piece_position_switch(og_square, destination_square)
-
-    pygame.display.flip()
+        
+    # Loop runs at 60 frames per second
     clock.tick(60)
     turn_count += 1
-
+    
+    # Blacks out screen to refresh whose turn and turn counter numbers
     screen.fill(black)
 
-# am i in check?
+### Functions to add: In-Check display ###
